3d180b554816f9bc0ef264fb80564ba11f346185
GabilaB <willibroad@naver.com>
2018-11-16 20:21:35 +0900

    first commit

---
+++ Dom scripting/DOMCart/js/app.js
@@ -0,0 +1,14 @@
+// let element;
+
+// element = document.forms[0].method;
+
+// console.log(element);
+
+// console.log(document.getElementById("heading"));
+
+// console.log(heading.textContent);
+
+// heading.style.background = "green";
+// heading.style.padding = "30px";
+
+const learningHeading = document.querySelector("#learn");
